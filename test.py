ade50b409d2aca196d6c9a81f0d947ac381e7f9c
fakela <favourkelvin17@gmail.com>
2021-01-21 13:44:33 +0100

    added  pagination working, across multiple sites

--- test.py
+++ test.py
@@ -4,45 +4,62 @@ import time
 import pandas as pd
 import numpy as np
 import re
-# import save_cookies
 
-#path to save the cookies
-cookies_location = "/Users/favourkelvin/Documents/recipe-links-crawler/cookies.txt"
 
 PATH = "/Users/favourkelvin/Downloads/chromedriver"
 driver = webdriver.Chrome(PATH)
-driver.get("https://www.allrecipes.com/search/results/?wt=meatballs&page=1")
-# save_cookies(driver, cookies_location)
-time.sleep(3)
-# find all recipes in the page
-
-incategory = driver.find_elements_by_class_name("fixed-recipe-card")
-#Generate a list of links for each and every recipe
-links = []
-for i in range(len(incategory)):
-	item = incategory[i]
-	#get the href property
-	a = item.find_element_by_tag_name("h3").find_element_by_tag_name("a").get_property("href")
-	#Append the link to list links
-	links.append(a)
+# parse the page source using get() function
+driver.get("https://www.allrecipes.com/search/results/?wt=meatballs&page={}")
 
 
-
+#next_button = driver.find_element_by_class_name("next").find_element_by_tag_name("a").click()
 all_details = []
-# loop through each link to acces the page of each book
-for link in links:
-	# get one url
-	driver.get(url=link)
-	# title of the recipe
-	elements = driver.find_elements_by_xpath("/html/body/div[2]/div/main/div[1]/div[2]/div[1]/div[1]/div[1]/div/h1")
-	Title = [el.text for el in elements]
-	# author of the recipe
-	elements = driver.find_elements_by_xpath("/html/body/div[2]/div/main/div[1]/div[2]/div[1]/div[1]/div[4]/div/span/span/a")
-	author = [el.text for el in elements]
-	# ingredient 
-	elements = driver.find_elements_by_xpath("//*[@id='ar-calvera-app']/section[1]/fieldset/ul/li/label/span/span")
-	ingredient = [el.text for el in elements]
-	# description comes as string
-	
-	print(Title, author, ingredient)
-	
+for c in range(1, 6):
+    try:
+        # get the page
+        driver.get(
+            "https://www.allrecipes.com/search/results/?wt=meatballs&page={}".format(c))
+        print(
+            "https://www.allrecipes.com/search/results/?wt=meatballs&page={}".format(c))
+
+        incategory = driver.find_elements_by_class_name("fixed-recipe-card")
+
+        links = []
+        for i in range(len(incategory)):
+            item = incategory[i]
+            # get the href property
+            a = item.find_element_by_tag_name(
+                "h3").find_element_by_tag_name("a").get_property("href")
+            # Append the link to list links
+            links.append(a)
+
+        # Lets loop through each link to acces the page of each recipe
+        recipe = []
+        for link in links:
+            
+            # get one recipe url
+            driver.get(url=link)
+            # title of the recipe
+            elements = driver.find_elements_by_xpath(
+                "/html/body/div[2]/div/main/div[1]/div[2]/div[1]/div[1]/div[1]/div/h1")
+            title = [el.text for el in elements]
+
+            # author
+            elements = driver.find_elements_by_xpath(
+                "/html/body/div[2]/div/main/div[1]/div[2]/div[1]/div[1]/div[4]/div/span/span/a")
+            author = [el.text for el in elements]
+
+            # ingredient
+            elements = driver.find_elements_by_xpath(
+                "//*[@id='ar-calvera-app']/section[1]/fieldset/ul/li/label/span/span")
+            ingredient = [el.text for el in elements]
+
+            r = (title, author, ingredient)
+            # append r to all details
+            recipe.append(r)
+        
+        
+    except:
+        driver.close()
+    print(recipe)    
+   
